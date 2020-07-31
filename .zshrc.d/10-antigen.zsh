2377d9866e9b4f8ac9cdc21758b82b5c4cb69a20
Stephen Lewis <code@stephenlewis.me>
2020-07-31 21:22:41 +0100

    Remove nvm antigen plugin
    That was very confusing.

--- .zshrc.d/10-antigen.zsh
+++ .zshrc.d/10-antigen.zsh
@@ -6,9 +6,6 @@ source $(brew --prefix)/share/antigen/antigen.zsh
 # ................................................
 # Declare the required bundles
 # ................................................
-# Automatically detect .nvmrc files on `cd`
-export NVM_AUTO_USE=true
-antigen bundle lukechilds/zsh-nvm
 antigen bundle zsh-users/zsh-syntax-highlighting
 
 # ................................................
