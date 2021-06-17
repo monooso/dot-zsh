5b4f7c00e6be84bcc5600a6248dfd7d8ed592be1
Stephen Lewis <code@stephenlewis.me>
2021-06-17 08:38:58 +0100

    Split antigen scripts into separate stages

--- .zshrc.d/10-antigen.zsh
+++ .zshrc.d/10-antigen.zsh
@@ -1,17 +1,4 @@
 # ------------------------------------------------
-# Antigen
+# Source antigen before adding bundles separately
 # ------------------------------------------------
 source $(brew --prefix)/share/antigen/antigen.zsh
-
-# ................................................
-# Declare the required bundles
-# ................................................
-antigen bundle \
-  zsh-users/zsh-autosuggestions \
-  zsh-users/zsh-completions \
-  zsh-users/zsh-syntax-highlighting
-
-# ................................................
-# Load the required bundles
-# ................................................
-antigen apply
---
+++ .zshrc.d/20-antigen-zsh-bundles.zsh
@@ -0,0 +1,7 @@
+# ................................................
+# Declare the ZSH antigen bundles
+# ................................................
+antigen bundle \
+  zsh-users/zsh-autosuggestions \
+  zsh-users/zsh-completions \
+  zsh-users/zsh-syntax-highlighting
