d68d8ff2b517665177635721a07470790b1c3f68
Stephen Lewis <code@stephenlewis.me>
2021-09-02 10:39:26 +0100

    Use $PWD, not ~

--- .zshrc.d/00-zsh.zsh
+++ .zshrc.d/00-zsh.zsh
@@ -12,7 +12,7 @@ zsh_cache=${HOME}/.zsh-cache
 mkdir -p $zsh_cache
 
 # Paths
-export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="$PWD/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # Prompt
